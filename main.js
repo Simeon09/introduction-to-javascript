2a220fb72b03f09746fdd29473dde6961cd5784e
unknown <simeonolawale69@gmail.com>
2022-03-14 12:51:27 +0100

    first javascript

---
+++ main.js
@@ -0,0 +1,25 @@
+var x = 5;
+var y = 6;
+var z = x + y;
+console .log(z)
+
+let student1="tolu"
+let student2="bunmi"
+console.log(student1, student2)
+
+
+let carName ='volvo';
+document.getElementById('demo').innerHTML= carName;
+// // WRITE A PROGRAM THAT AS ASK A USER OF HIS OR HER NAME, 
+// AGE , ADDRESS AND PHONE NUMBER THEN UR  PROGRAM TO DISPLAY D 
+// VALUES ON UR HTML PAGE 
+ 
+var question = prompt('Enter your Name');
+var age =prompt('Enter your age');
+var address =prompt('Enter your home address bossteeh');
+var phoneNumber =prompt('Enter your phoneNumber bossteeh');
+document.getElementById('demo').innerHTML= question;
+document.getElementById('age').innerHTML=age;
+console.log
+document.getElementById('address').innerHTML=address;
+document.getElementById('phoneNumber').innerHTML= phoneNumber;
