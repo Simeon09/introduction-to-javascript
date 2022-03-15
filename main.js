f3ac0edcc574f30312228b457567bb22b122be58
unknown <simeonolawale69@gmail.com>
2022-03-15 10:14:30 +0100

    ...

--- main.js
+++ main.js
@@ -18,14 +18,25 @@ var question = prompt('Enter your Name');
 var age =prompt('Enter your age');
 var address =prompt('Enter your home address bossteeh');
 var phoneNumber =prompt('Enter your phoneNumber bossteeh');
+var Second= prompt('how many seconds make a minute');
+var Minute= prompt('how many minute make an hour');
+var Hour = prompt('how many hour make a day');
+var Days= prompt('how many days make a week');
+
 document.getElementById('demo').innerHTML= question;
 document.getElementById('age').innerHTML=age;
 
 document.getElementById('address').innerHTML=address;
 document.getElementById('phoneNumber').innerHTML= phoneNumber;
+document.getElementById('seconds').innerHTML= Second;
+document.getElementById('minute').innerHTML= Minute;
+document.getElementById('hour').innerHTML= Hour;
+document.getElementById('day').innerHTML= Days;
+
 
 let myname = "simeon"
 alert ('welcome to ATC' + question)
+alert('welcome to my small calculator'  +  question)
 // write a program to calculate the number 
 // of seconds in a week and display ur answer on d html webpage.
 var seconds= 60;
@@ -37,3 +48,4 @@ var day = hour * p;
 var week = day *d;
 console.log(week)
 document.getElementById('week').innerHTML= week;
+//var Second= prompt('how many seconds make a minute');
